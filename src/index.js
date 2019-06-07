8931d602b08e9bd33d8a7c2c77cfaed0857977be
Bennie van der Wel <bennievanderwel@gmail.com>
2019-06-07 12:33:33 +0200

    Added materialize css, node modules, sass variables, and normalize

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'index.css';
+import 'styles/styles.scss';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
